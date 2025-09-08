7c46a09825e5c42e25a70fbe47c1e50dcb316c25
caleb-wo <camawolfe52@icloud.com>
2025-09-07 18:43:38 -0600

    Begin lesson: Update the UI with State

---
+++ DiceRoller/DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Caleb Wolfe on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
