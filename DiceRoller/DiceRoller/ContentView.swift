902088aa3006b2220edf9e383e720244180761ee
caleb-wo <camawolfe52@icloud.com>
2025-09-08 20:56:54 -0600

    55% progress

--- DiceRoller/DiceRoller/ContentView.swift
+++ DiceRoller/DiceRoller/ContentView.swift
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice = 1
+    @State private var dividedByZero = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.largeTitle
+                      .lowercaseSmallCaps()
+                      .bold())
+            HStack{
+                ForEach(1...numberOfDice, id: \.description){_ in
+                    DiceView()
+                }
+            }
+            
+            HStack{
+                Spacer()
+                Button("Add Dice"){
+                    numberOfDice += 1
+                }
+                .padding()
+                .background(.green ,
+                            in :RoundedRectangle(cornerRadius: 10))
+                .foregroundStyle(.white)
+                
+                Button("Remove Dice"){
+                    if (numberOfDice > 1){
+                        numberOfDice -= 1
+                    } else{
+                        dividedByZero = true
+                    }
+                }
+                .padding()
+                .background(.red,
+                            in :RoundedRectangle(cornerRadius: 10))
+                .foregroundStyle(.white)
+                Spacer()
+            }
         }
         .padding()
+        .alert("Can't Remove",
+               isPresented: $dividedByZero){
+                Button("Okay"){}
+            } message: {
+                Text("You must have one die.")
+            }
+        
     }
 }
 
