b11072ba606d4e70dd4d24121802258257bf1826
caleb-wo <camawolfe52@icloud.com>
2025-09-09 22:26:54 -0600

    Begin lesson: "Create dynamic content."

---
+++ Pick-a-Pal/Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Caleb Wolfe on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
