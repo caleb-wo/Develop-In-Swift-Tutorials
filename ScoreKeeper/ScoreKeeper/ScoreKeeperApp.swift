e8ae885a6cfba8f95292e8594d38fb038068c3c5
caleb-wo <camawolfe52@icloud.com>
2025-09-13 13:25:33 -0600

    Begin lesson: "Model data with custom types."

---
+++ ScoreKeeper/ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Caleb Wolfe on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
