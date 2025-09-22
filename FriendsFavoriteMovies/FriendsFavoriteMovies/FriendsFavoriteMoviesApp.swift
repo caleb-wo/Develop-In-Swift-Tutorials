4b5aec993a405ef044b0077ada855e75e0601214
caleb-wo <camawolfe52@icloud.com>
2025-09-22 13:01:00 -0600

    Begin lesson: "Navigate sample data."

---
+++ FriendsFavoriteMovies/FriendsFavoriteMovies/FriendsFavoriteMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFavoriteMoviesApp.swift
+//  FriendsFavoriteMovies
+//
+//  Created by Caleb Wolfe on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFavoriteMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
